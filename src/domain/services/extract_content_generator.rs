e1135ab5af38c1a0420e48b14f692eb8ee995d63
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-01-29 12:18:47 +0100

    feat: first step towards an extract content generator

--- src/domain/services/extract_content_generator.rs
+++ src/domain/services/extract_content_generator.rs
@@ -1,3 +1,5 @@
+use std::io::{BufRead, BufReader, Read};
+
 use genawaiter::{rc::gen, yield_, GeneratorState};
 
 // TODO: HERE: use a generator to yield progressively the extracted content of the epub file
@@ -37,81 +39,106 @@ const HTML_ELEMENTS_NEEDING_A_SPACE: [&str; 11] = [
 /// let extracted_content = extract_content(content);
 /// assert_eq!(extracted_content, "Test");
 /// ```
-pub fn extract_content(epub_content: &str) -> String {
+pub fn extract_content_generator<ContentToReadType>(
+    buf_reader: BufReader<ContentToReadType>,
+) -> String
+where
+    ContentToReadType: std::io::Read,
+{
     // let mut current_html_elements = std::collections::HashMap::<String, u32>::new();
-    let mut extracted_content = String::new();
+    let mut current_extracted_content = String::new();
 
     let mut is_inside_body = false;
     let mut might_be_html_element = false;
     let mut previous_char_is_an_opening = false;
 
+    // Stores a possible currently being extracted html element to:
+    // - check if it is a `body` or any other html element
+    // - or to add its content to the current_extracted_content if it's not an html element
     let mut current_possible_html_element = String::new();
 
-    // Reads characters one by one
+    // Reads lines one by one, and for each line, read characters one by one
     // TODO: careful/check when none utf-8 characters
-    // Only keep the characters that are not inside an HTML element
-    // But inside the body element
-    for c in epub_content.chars() {
-        if might_be_html_element {
-            // An opening < has already been seen. If among the next characters there is a new opening <,
-            // then the string since the previous < is part of the content of the epub. And the new < could be an html element.
-            if c == '<' {
+    // Extracted content: only keeps the characters that are not HTML elements
+    // and that are contained inside the <body> elements
+    // Performance: O(n) where n is the number of characters in the epub content
+    for line in buf_reader.lines() {
+        let line = line.unwrap();
+        println!("🥐 line: {}", line);
+        for c in line.chars() {
+            if might_be_html_element {
+                // An opening < has already been seen. If among the next characters there is a new opening <,
+                // then the string since the previous < is part of the content of the epub.
+                // And the new < could be an html element.
+                if c == '<' {
+                    previous_char_is_an_opening = true;
+                    if is_inside_body {
+                        current_extracted_content.push('<');
+                        current_extracted_content.push_str(&current_possible_html_element);
+                    }
+
+                    current_possible_html_element = String::new();
+                } else if c == '>' {
+                    if current_possible_html_element == "body" {
+                        is_inside_body = true;
+                    } else if current_possible_html_element == "/body" {
+                        is_inside_body = false;
+                    }
+
+                    // Adds a space if the HTML element is an opening paragraph, a header etc.
+                    if is_inside_body
+                        && HTML_ELEMENTS_NEEDING_A_SPACE
+                            .contains(&current_possible_html_element.as_str())
+                        && current_extracted_content.len() > 0
+                        && !current_extracted_content.ends_with(' ')
+                    {
+                        current_extracted_content.push(' ');
+                    }
+
+                    // TODO: handles the case where there is a < and a > but it's actual content
+
+                    might_be_html_element = false;
+                    previous_char_is_an_opening = false;
+                    current_possible_html_element = String::new();
+                }
+                // A space is only possible at the end of an HTML element
+                else if previous_char_is_an_opening && c == ' ' {
+                    might_be_html_element = false;
+                    previous_char_is_an_opening = false;
+
+                    // The previous string is part of the content of the epub
+                    if is_inside_body {
+                        current_extracted_content.push_str(&current_possible_html_element);
+                    }
+                    current_possible_html_element = String::new();
+                } else {
+                    current_possible_html_element.push(c);
+                    previous_char_is_an_opening = false;
+                }
+            }
+            // Needs to check if < represents the beginning of an HTML element, or if it's a simple character in the content
+            else if c == '<' {
+                // The case with a second < is handled in the might_be_html_element block
                 previous_char_is_an_opening = true;
-                if is_inside_body {
-                    extracted_content.push('<');
-                    extracted_content.push_str(&current_possible_html_element);
-                }
-
-                current_possible_html_element = String::new();
-            } else if c == '>' {
-                if current_possible_html_element == "body" {
-                    is_inside_body = true;
-                } else if current_possible_html_element == "/body" {
-                    is_inside_body = false;
-                }
-
-                // Adds a space if the HTML element is am opening paragraph, a header etc.
-                if is_inside_body
-                    && HTML_ELEMENTS_NEEDING_A_SPACE
-                        .contains(&current_possible_html_element.as_str())
-                    && extracted_content.len() > 0
-                    && !extracted_content.ends_with(' ')
+                might_be_html_element = true;
+            } else if is_inside_body {
+                // Avoids adding unecessary spaces
+                if c != ' '
+                    || (current_extracted_content.len() > 0
+                        && !current_extracted_content.ends_with(' '))
                 {
-                    extracted_content.push(' ');
+                    current_extracted_content.push(c);
                 }
-
-                // TODO: handles the case where there is a < and a > but it's actual content
-
-                might_be_html_element = false;
-                previous_char_is_an_opening = false;
-                current_possible_html_element = String::new();
             }
-            // A space is only possible at the end of an HTML element
-            else if previous_char_is_an_opening && c == ' ' {
-                might_be_html_element = false;
-                previous_char_is_an_opening = false;
-
-                // The previous string is part of the content of the epub
-                if is_inside_body {
-                    extracted_content.push_str(&current_possible_html_element);
-                }
-                current_possible_html_element = String::new();
-            } else {
-                current_possible_html_element.push(c);
-                previous_char_is_an_opening = false;
-            }
-        }
-        // Needs to check if < represents the beginning of an HTML element, or if it's a simple character in the content
-        else if c == '<' {
-            // The case with a second < is handled in the might_be_html_element block
-            previous_char_is_an_opening = true;
-            might_be_html_element = true;
-        } else if is_inside_body {
-            extracted_content.push(c);
         }
     }
 
-    extracted_content
+    // Removes last space if there is one
+    if current_extracted_content.len() > 0 && current_extracted_content.ends_with(' ') {
+        current_extracted_content.pop();
+    }
+
+    current_extracted_content
 }
 
 #[cfg(test)]
@@ -123,11 +150,28 @@ mod tests {
     use std::io::{BufRead, BufReader, Read};
 
     speculate! {
-        describe "extract_content" {
+        describe "extract_content_generator" {
             describe "On a simple and correct EPUB content" {
                 it "it should extract the content correctly" {
                     let content = "<html><head><title>Test</title></head><body><p>Test</p></body></html>";
-                    let extracted_content = extract_content(content);
+                    let buf_reader = BufReader::new(content.as_bytes());
+                    let extracted_content = extract_content_generator(buf_reader);
+                    assert_eq!(extracted_content, "Test");
+                }
+            }
+
+
+            describe "On a multiline and correct EPUB content" {
+                it "it should extract the content correctly" {
+                    let content = "\
+<html>
+    <head><title>Test</title></head>
+    <body>
+        <p>Test</p>
+    </body>
+</html>";
+                    let buf_reader = BufReader::new(content.as_bytes());
+                    let extracted_content = extract_content_generator(buf_reader);
                     assert_eq!(extracted_content, "Test");
                 }
             }
@@ -145,7 +189,8 @@ mod tests {
                     println!("🦀");
                     println!("result simple_1: {}", result);
 
-                    let extracted_content = extract_content(&content);
+                    let buf_reader = BufReader::new(content.as_bytes());
+                    let extracted_content = extract_content_generator(buf_reader);
                     println!("💙");
                     println!("extracted content simple_1: {}", extracted_content);
                     assert_eq!(extracted_content, result);
