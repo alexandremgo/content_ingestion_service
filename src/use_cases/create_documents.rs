e1aa14c2c7fbeb8c3682abed13285f3525f46199
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-04-10 09:49:22 +0200

    feat: create_document use case - wip

--- src/use_cases/create_documents.rs
+++ src/use_cases/create_documents.rs
@@ -1,7 +1,12 @@
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::io::{BufRead, BufReader};
+
+use genawaiter::GeneratorState;
 
 use crate::domain::entities::document::Document;
 use crate::domain::entities::epub_file::EpubFile;
+use crate::domain::services::extract_content_generator::extract_content_generator;
 
 const DEFAULT_NB_WORDS_PER_DOCUMENT: usize = 200;
 
@@ -15,21 +20,62 @@ pub struct CreateDocumentsArgs {
 }
 
 pub fn create_documents(args: CreateDocumentsArgs) -> Result<(), ()> {
-    let CreateDocumentsArgs { epub_file, nb_words_per_document } = args;
+    let CreateDocumentsArgs {
+        epub_file,
+        nb_words_per_document,
+    } = args;
     let nb_words_per_document = nb_words_per_document.unwrap_or(DEFAULT_NB_WORDS_PER_DOCUMENT);
 
-    // For the actual API: will we save the epub file when receiving it ?
+    // TODO: For the actual API: will we save the epub file when receiving it ?
     // For now, no repository for EpubFile. We will just read it from the filesystem
+    // TODO: we're opening the EPUB as a file , is this what we want ?
     let mut file = std::fs::File::open(epub_file.path).unwrap();
 
-    let file_reader = BufReader::new(file);
-    let mut lines_iter = file_reader.lines();
+    let buf_reader = BufReader::new(file);
 
     // Without having a state machine, we can't know if we are in the body or not
 
-    // Could give the BufReader to the services extract_content, and let it handle the state machine 
+    // ? Could give the BufReader to the services extract_content, and let it handle the state machine
     // It would follow a generator pattern: yielding the content, each time it reaches nb_words_per_document
+    let mut generator = extract_content_generator(buf_reader, Some(nb_words_per_document));
+    let mut completed = false;
+
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open("src/tests/results.txt")
+        .unwrap();
+
+    while !completed {
+        let extracted_content = match generator.as_mut().resume() {
+            GeneratorState::Yielded(content) => content,
+            GeneratorState::Complete(_result) => {
+                completed = true;
+                String::from("")
+            }
+        };
+
+        if let Err(e) = writeln!(file, "{}\n----\n", extracted_content) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+    }
 
     // Should i build an aggregate entity that would create all the documents ?
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::use_cases::create_documents::{create_documents, CreateDocumentsArgs};
+    use crate::domain::entities::epub_file::EpubFile;
+
+    #[test]
+    fn test_working() {
+        let epub_file = EpubFile { book_id: String::from(""), path: String::from("src/tests/accessible_epub_3.epub") };
+
+        if let Err(e) = create_documents(CreateDocumentsArgs { epub_file, nb_words_per_document: Some(8) }) {
+            panic!("Error: {:?}", e);
+        }
+        assert_eq!(1, 1);
+    }
 }
