9a16733052ec927cf6b9ec7c267c5b7cb14b9c8a
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-05-10 20:53:23 +0200

    wip: work on t_describe macro and testing

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,7 +1,8 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use syn::{
-    parse::{Parse, ParseStream, Result},
+    parse::{Parse, ParseStream},
     parse_macro_input,
     punctuated::Punctuated,
     ItemFn, Lit, Token,
@@ -14,7 +15,7 @@ struct Args {
 }
 
 impl Parse for Args {
-    fn parse(input: ParseStream) -> Result<Self> {
+    fn parse(input: ParseStream) -> syn::parse::Result<Self> {
         //
         let vars = Punctuated::<Lit, Token![,]>::parse_terminated(input)?;
         Ok(Args {
@@ -26,7 +27,7 @@ impl Parse for Args {
 // TODO: setup log env
 // TODO: handles if there are other macros:
 // For ex:
-// 
+//
 // #[t_describe(
 //     "On a more complex and correct EPUB content",
 //     "it should extract the content correctly in 1 yield"
@@ -35,51 +36,86 @@ impl Parse for Args {
 // fn test() { ... }
 #[proc_macro_attribute]
 pub fn t_describe(attr: TokenStream, item: TokenStream) -> TokenStream {
-    // Parse the attribute arguments.
+    // Parses the attribute arguments.
     let attr_args = parse_macro_input!(attr as Args);
 
+    // Parses the elements on which the macro is applied to
+    let input = parse_macro_input!(item as ItemFn);
+
+    t_describe_implementation(&attr_args, &input)
+        .unwrap()
+        .into()
+}
+
+fn t_describe_implementation(attr_args: &Args, item_fn: &ItemFn) -> Result<TokenStream2, String> {
     if attr_args.vars.len() < 1 {
         panic!("Describe the test in at least one sentence");
     }
 
-    let description: Vec<String> = attr_args
+    let description = attr_args
         .vars
         .iter()
         .map(|literal| {
             if let Lit::Str(lit_str) = literal {
-                lit_str.value()
+                Ok(lit_str.value())
             } else {
-                panic!("The args should only be string");
+                return Err("The args should only be string".to_string());
             }
         })
-        .collect();
+        .collect::<Result<Vec<String>, String>>()?;
 
     let description = description.join("\n");
     println!("Ok {description}");
 
-    let input = parse_macro_input!(item as ItemFn);
-
     // Extracts the name and block of the input function.
-    let fn_name = input.sig.ident;
+    let fn_name = &item_fn.sig.ident;
     println!("Signature: 🦖 {:?}", fn_name);
-    let fn_block = input.block;
+    let fn_block = &item_fn.block;
 
-    quote! {
+    let result = quote! {
         fn #fn_name() {
             println!(#description);
             #fn_block
         }
-    }
-    .into()
+    };
+
+    Ok(result)
 }
 
-// #[cfg(test)]
-// mod test {
-//     use super::*;
+#[cfg(test)]
+mod test_macro {
+    use super::*;
+    use syn::{parse2, ItemFn};
 
-//     #[test]
-//     #[t_describe("ok")]
-//     fn it_should_print_description() {
+    #[test]
+    fn it_should_print_description() {
+        let input_token_stream = quote! {
+            fn tested_fn(param1: usize) -> Result<(), ()> {
+                let result = doing_something(param1);
+                Ok(())
+            }
+        };
 
-//     }
-// }
+        let expected_token_stream = quote! {
+            fn tested_fn(param1: usize) -> Result<(), ()> {
+                println!("Test text");
+                let result = doing_something(param1);
+                Ok(())
+            }
+        };
+
+        let input = parse2::<ItemFn>(input_token_stream).unwrap();
+
+        let attr = quote! {
+            "Test text"
+        };
+
+        let attr_args = parse2::<Args>(attr).unwrap();
+
+        let result = t_describe_implementation(&attr_args, &input).unwrap();
+
+        println!("Result: {}", result.to_string());
+
+        assert_eq!(result.to_string(), expected_token_stream.to_string());
+    }
+}
