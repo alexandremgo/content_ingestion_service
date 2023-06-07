e8c16c921da900b509827f486095a13a0aee7525
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-06-07 15:27:38 +0200

    feat: telemetry and error handling in worker queue message handler

---
+++ content_ingestion_worker/src/configuration.rs
@@ -0,0 +1,82 @@
+use serde_aux::field_attributes::deserialize_number_from_string;
+
+#[derive(serde::Deserialize, Clone)]
+pub struct Settings {
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize, Clone)]
+pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
+    pub host: String,
+}
+
+/// Extracts app settings from configuration files and env variables
+/// 
+/// `base.yaml` should contain shared settings for all environments.
+/// A specific env file should be created for each environment: `local.yaml` and `production.yaml`
+/// The environment is set with the env var `APP_ENVIRONMENT`. 
+/// If `APP_ENVIRONMENT` is not set, `local.yaml` is the default.
+/// 
+/// Settings are also taken from environment variables: with a prefix of APP and '__' as separator
+/// For ex: `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    // Detects the running environment.
+    // Default to `local` if unspecified.
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+    let environment_filename = format!("{}.yaml", environment.as_str());
+
+    let settings = config::Config::builder()
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
+        ))
+        // Adds in settings from environment variables (with a prefix of APP and '__' as separator)
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
+        .build()?;
+
+    settings.try_deserialize::<Settings>()
+}
+
+/// The possible runtime environment for our application.
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
+}
--- content_ingestion_worker/src/main.rs
+++ content_ingestion_worker/src/main.rs
@@ -1,12 +1,62 @@
+use content_ingestion_worker::{
+    helper::error_chain_fmt,
+    telemetry::{get_tracing_subscriber, init_tracing_subscriber},
+};
 use lapin::{
     message::DeliveryResult,
     options::{BasicAckOptions, BasicConsumeOptions, BasicPublishOptions, QueueDeclareOptions},
     types::FieldTable,
     BasicProperties, Connection, ConnectionProperties,
 };
+use tracing::{error, info, info_span, Instrument};
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct MyData {
+    field_1: String,
+    field_2: String,
+}
+
+#[derive(thiserror::Error)]
+pub enum MyDataParsingError {
+    #[error("Data could not be converted from utf8 u8 vector to string")]
+    InvalidStringData(#[from] std::str::Utf8Error),
+
+    #[error("Data did not represent a valid JSON object: {0}")]
+    InvalidJsonData(#[from] serde_json::Error),
+}
+
+impl std::fmt::Debug for MyDataParsingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl MyData {
+    fn try_parsing(data: &Vec<u8>) -> Result<Self, MyDataParsingError> {
+        let data = std::str::from_utf8(data)?;
+        let my_data = serde_json::from_str(data)?;
+
+        Ok(my_data)
+    }
+}
+
+#[tracing::instrument(name = "Handling queued job")]
+fn handler(my_data: MyData) -> Result<(), String> {
+    // Do something with the delivery data (The message payload)
+    info!("🍕 Received data: {:?}\n", my_data);
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
+    let tracing_subscriber = get_tracing_subscriber(
+        "content_ingestion_worker".into(),
+        "info".into(),
+        std::io::stdout,
+    );
+    init_tracing_subscriber(tracing_subscriber);
+
     let uri = "amqp://localhost:5672";
     let options = ConnectionProperties::default()
         // Use tokio executor and reactor.
@@ -36,43 +86,75 @@ async fn main() {
         .await
         .unwrap();
 
-    consumer.set_delegate(move |delivery: DeliveryResult| async move {
-        let delivery = match delivery {
-            // Carries the delivery alongside its channel
-            Ok(Some(delivery)) => delivery,
-            // The consumer got canceled
-            Ok(None) => return,
-            // Carries the error and is always followed by Ok(None)
-            Err(error) => {
-                dbg!("Failed to consume queue message {}", error);
-                return;
+    consumer.set_delegate(move |delivery: DeliveryResult| {
+        async move {
+            let delivery = match delivery {
+                // Carries the delivery alongside its channel
+                Ok(Some(delivery)) => delivery,
+                // The consumer got canceled
+                Ok(None) => return,
+                // Carries the error and is always followed by Ok(None)
+                Err(error) => {
+                    error!(?error, "Failed to consume queue message");
+                    return;
+                }
+            };
+
+            let my_data = match MyData::try_parsing(&delivery.data) {
+                Ok(my_data) => my_data,
+                Err(error) => {
+                    error!(?error, "Failed to parse queue message data: {}", error);
+                    return;
+                }
+            };
+
+            info!(
+                "🦖 Received message properties: {:#?}\n",
+                delivery.properties
+            );
+
+            match handler(my_data) {
+                Ok(()) => (),
+                Err(error) => {
+                    error!(?error, "Failed to handle queue message");
+                    return;
+                }
             }
-        };
 
-        // Do something with the delivery data (The message payload)
-        println!("🍕Received message: {}\n", std::str::from_utf8(&delivery.data).unwrap());
-        println!("🐬Received message: {:?}\n", delivery.data);
-
-        // Do something with the delivery properties (The message properties)
-        println!("🦖Received message properties: {:#?}\n", delivery.properties);
-
-
-        delivery
-            .ack(BasicAckOptions::default())
-            .await
-            .expect("Failed to ack send_webhook_event message");
+            delivery
+                .ack(BasicAckOptions::default())
+                .await
+                .expect("Failed to ack send_webhook_event message");
+        }
+        .instrument(info_span!(
+            "Handling queued message",
+            handler_id = %uuid::Uuid::new_v4()
+        ))
     });
 
-    channel
-        .basic_publish(
-            "",
-            "queue_test",
-            BasicPublishOptions::default(),
-            b"Hello world!",
-            BasicProperties::default(),
-        )
-        .await
-        .unwrap()
-        .await
-        .unwrap();
+    loop {
+        let my_data = MyData {
+            field_1: "test".to_string(),
+            field_2: "ok".to_string(),
+        };
+        let my_data = serde_json::to_string(&my_data).unwrap();
+        let current_time_ms = chrono::Utc::now().timestamp_millis() as u64;
+
+        channel
+            .basic_publish(
+                "",
+                "queue_test",
+                BasicPublishOptions::default(),
+                &my_data.as_bytes(),
+                BasicProperties::default()
+                    .with_timestamp(current_time_ms)
+                    .with_message_id(uuid::Uuid::new_v4().to_string().into())
+            )
+            .await
+            .unwrap()
+            .await
+            .unwrap();
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    }
 }
