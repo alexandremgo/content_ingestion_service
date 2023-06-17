8d09a45e6c6df714de413472fb4e59cf54c37cca
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-06-17 11:45:37 +0200

    feat: 400 error when input data is missing
    For add_source_files route

--- content_ingestion_service/tests/api/add_source_files.rs
+++ content_ingestion_service/tests/api/add_source_files.rs
@@ -25,3 +25,24 @@ async fn add_source_files_returns_a_200_for_valid_input_data() {
     // Assert
     assert_eq!(200, response.status().as_u16());
 }
+
+
+#[tokio::test]
+async fn add_source_files_returns_a_400_when_input_data_is_missing() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Creates a form without any multipart field
+    let form = Form::new();
+
+    // Act
+    let response = reqwest::Client::new()
+        .post(&format!("{}/add_source_files", &app.address))
+        .multipart(form)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(400, response.status().as_u16());
+}
