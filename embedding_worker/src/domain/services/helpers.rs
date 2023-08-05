7cb521d9677194abcd31f25cfba168b6af8df74c
Alexandre Magaud <alexandre.magaud@gmail.com>
2023-08-05 16:37:50 +0200

    chore: test on 1 simple sentence + documentation

--- embedding_worker/src/domain/services/helpers.rs
+++ embedding_worker/src/domain/services/helpers.rs
@@ -1,5 +1,5 @@
-use regex::Regex;
 use once_cell::sync::Lazy;
+use regex::Regex;
 
 /// Simple sentences splitter.
 ///
@@ -7,8 +7,10 @@ use once_cell::sync::Lazy;
 /// - groups sentences finishing by .?! or if reaching the end of the content.
 /// - removes sentences with less than 2 characters (ex: a `!` from several `!!!`)
 pub fn split_sentences(content: &str) -> Vec<String> {
-    // let re = Regex::new(r"\s*(?P<sentence>[^.!?]*(?:[.!?]|$))").unwrap();
-    static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\s*(?P<sentence>[^.!?]*(?:[.!?]|$))").unwrap());
+    // Panics if the regex cannot be built
+    static RE: Lazy<Regex> =
+        Lazy::new(|| Regex::new(r"\s*(?P<sentence>[^.!?]*(?:[.!?]|$))").unwrap());
+
     RE.captures_iter(content)
         .map(|cap| cap["sentence"].to_string())
         .filter(|sentence| sentence.len() > 1)
@@ -18,6 +20,13 @@ pub fn split_sentences(content: &str) -> Vec<String> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn on_one_simple_sentence_it_returns_the_sentence() {
+        let content = "Hello world, it's the end";
+        let sentences = split_sentences(content);
+        assert_eq!(sentences, vec!["Hello world, it's the end",])
+    }
 
     #[test]
     fn on_simple_sentences_it_splits_correctly() {
